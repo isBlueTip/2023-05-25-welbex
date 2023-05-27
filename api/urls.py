066d794c0b33055d743ca4c76a99607336282a9f
Simon Egorov <rhinoRofl@gmail.com>
2023-05-27 12:07:00 +0600

    Create api urls

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from cargo.views import CargoViewSet
+from trucks.views import TruckViewSet
+
+app_name = "api"
+router = routers.DefaultRouter()
+router.register("cargo", CargoViewSet, basename="cargo")
+router.register("trucks", TruckViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
